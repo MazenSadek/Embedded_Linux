956ef7d519d46e0a27cf00a1de1c1d4d5eb799a0
MazenSadek <67530477+MazenSadek@users.noreply.github.com>
2023-09-18 00:12:45 +0300

    upload calculate accumulate task

---
+++ CPP/task_session_3/calculate_accumulate_of_array.cpp
@@ -0,0 +1,16 @@
+// #include <iostream>
+// #include <numeric>
+
+// int main() {
+//     int arr[] = {1, 2, 3, 4, 5};
+//     int size = sizeof(arr) / sizeof(arr[0]);
+    
+//     int sum = std::accumulate(arr, arr + size, 0);
+    
+//     std::cout << "Sum: " << sum << std::endl;
+    
+//     return 0;
+// }
+
+
+
